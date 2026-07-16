961d9c262abfc2647e9b8635f90540b362553174
rahul05arora <rahularora.cn@gmail.com>
2026-07-15 20:59:47 -0400

    Authentication & Multi-Screen Stack Navigation.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { type Request, type Response, type NextFunction } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
